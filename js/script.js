09457386d29b76e728989801a1bb5a3a1ed0933f
Emil-Ka <emilkaraev55@gmail.com>
2021-12-03 15:47:37 +0400

    programm for movies

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
-console.log("lorem")
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+const personalMovieDB = {
+   count: numberOfFilms,
+   movies: {},
+   actors: {},
+   genres: [],
+   privat: false
+};
+
+for (let i = 0; i < 2; i++) {
+   let lastFilm = prompt("Один из последних просмотренных фильмов?", "");
+   let reviewFilm = +prompt("На сколько оцените его?", "");
+   personalMovieDB.movies[lastFilm] = reviewFilm;
+}
+
+console.log(personalMovieDB);
