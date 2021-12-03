af9c16b3ffe11aa468bc4f45a4c73e528cf1dc85
Emil-Ka <emilkaraev55@gmail.com>
2021-12-03 16:17:20 +0400

    switch case

---
+++ js/switch.js
@@ -0,0 +1,14 @@
+let num = 23;
+switch (num) {
+   case 45:
+      console.log("это 45");
+      break;
+   case 50:
+      console.log("это 50");
+      break;
+   default:
+      console.log("no675ooo");
+      break;
+}
+
+(num === 34) ? console.log("yut") : console.log("dab dab");
